092b512c3868f29ce3c66e603e3864bba9fd4d72
John Renz <johnwrenz@gmail.com>
2017-11-21 10:42:51 -0600

    My final working code.

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -4,17 +4,41 @@ using TechJobs.Models;
 
 namespace TechJobs.Controllers
 {
-    public class SearchController : Controller
-    {
-        public IActionResult Index()
-        {
-            ViewBag.columns = ListController.columnChoices;
-            ViewBag.title = "Search";
-            return View();
-        }
+	
+	public class SearchController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewBag.columns = ListController.columnChoices;
+			ViewBag.title = "Search";
+			return View();
+		}
 
-        // TODO #1 - Create a Results action method to process 
-        // search request and display results
+		// TODO #1 - Create a Results action method to process 
+		// search request and display results
 
-    }
+		public IActionResult Results(string searchType, string searchTerm)
+
+		{
+			ViewBag.columns = ListController.columnChoices;
+			List < Dictionary < string, string >> Jobs;
+			if (searchType.Equals("all"))
+			{
+				Jobs = JobData.FindByValue(searchTerm);
+			
+			
+			}
+			else
+			{
+				Jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+			
+
+			}
+				ViewBag.jobs = Jobs; 
+				return View("Index");
+
+			
+		}
+	}
 }
+
